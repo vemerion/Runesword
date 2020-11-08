b552f1e0700899f99ebdcab84280dcf17c4a3a36
vemerion <vemerion@gmail.com>
2020-11-08 17:44:55 +0100

    Update runeforge block texture

--- src/main/java/mod/vemerion/runesword/block/RuneforgeBlock.java
+++ src/main/java/mod/vemerion/runesword/block/RuneforgeBlock.java
@@ -11,32 +11,40 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 public class RuneforgeBlock extends Block {
+	private static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 15.0D, 16.0D);
 
 	public RuneforgeBlock(Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return SHAPE;
 	}
 	
 	@Override
 	public boolean hasTileEntity(BlockState state) {
 		return true;
 	}
-	
+
 	@Override
 	public TileEntity createTileEntity(BlockState state, IBlockReader world) {
 		return new RuneforgeTileEntity();
 	}
-	
+
 	@Override
 	public INamedContainerProvider getContainer(BlockState state, World worldIn, BlockPos pos) {
 		TileEntity tileentity = worldIn.getTileEntity(pos);
 		return tileentity instanceof INamedContainerProvider ? (INamedContainerProvider) tileentity : null;
 	}
-	
+
 	@Override
 	public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player,
 			Hand handIn, BlockRayTraceResult hit) {
@@ -45,9 +53,9 @@ public class RuneforgeBlock extends Block {
 			if (tileentity != null) {
 				INamedContainerProvider containerProvider = getContainer(state, worldIn, pos);
 				NetworkHooks.openGui((ServerPlayerEntity) player, containerProvider);
-			}				
+			}
 		}
 		return ActionResultType.SUCCESS;
-	} 
+	}
 
 }
