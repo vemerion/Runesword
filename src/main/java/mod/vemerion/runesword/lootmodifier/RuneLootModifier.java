64e2d6eae5e7cc9dc8400025b345a47e23b6ac53
vemerion <vemerion@gmail.com>
2020-12-26 12:29:43 +0100

    Add zh_tw lang file (courtesy Pancakes0228)

--- src/main/java/mod/vemerion/runesword/lootmodifier/RuneLootModifier.java
+++ src/main/java/mod/vemerion/runesword/lootmodifier/RuneLootModifier.java
@@ -12,6 +12,15 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
 
+
+/* TODO:
+ * It turns out there is a corner case with using the RandomChanceWithLooting LootCondition with global loot modifier,
+ * since then the DamageSource will be null if there is no entity (and this might happen because every loot modifier is run after every block destroyed etc)
+ * Normally this is not a problem, but if another mod subscribes to the LootingLevelEvent and assumes that the DamageSource is not null,
+ * there might be a null pointer exception.
+ * Easy fix: Replace RandomChanceWithLooting with RandomChance in all loot modifier jsons
+ * More involved fix: Create a new loot condition that works around the problem?
+ */
 public class RuneLootModifier extends LootModifier {
 
 	private Item rune;
