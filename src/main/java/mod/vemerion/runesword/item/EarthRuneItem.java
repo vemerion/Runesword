0e147541e62de2f0ba13049979d145ea952e2a6c
vemerion <vemerion@gmail.com>
2021-01-14 16:24:27 +0100

    EarthRune now enchantable with fire aspect

--- src/main/java/mod/vemerion/runesword/item/EarthRuneItem.java
+++ src/main/java/mod/vemerion/runesword/item/EarthRuneItem.java
@@ -2,9 +2,11 @@ package mod.vemerion.runesword.item;
 
 import java.awt.Color;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantments;
@@ -12,14 +14,20 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
 
 public class EarthRuneItem extends RuneItem {
 
 	private static final List<Item> DROPS = ImmutableList.of(Items.IRON_ORE, Items.COAL_ORE);
+
+	private static final Set<Enchantment> ENCHANTS = ImmutableSet.of(Enchantments.FORTUNE, Enchantments.FIRE_ASPECT);
 
 	public EarthRuneItem(Properties properties) {
 		super(new Color(100, 50, 0).getRGB(), properties);
@@ -40,18 +48,29 @@ public class EarthRuneItem extends RuneItem {
 		if (!major && player.getRNG().nextDouble() < runes.size() * 0.1
 				+ getEnchantmentLevel(Enchantments.FORTUNE, runes) * 0.02) {
 			ItemEntity dirt = new ItemEntity(player.world, entityLiving.getPosX(), entityLiving.getPosY(),
-					entityLiving.getPosZ(), new ItemStack(getDrop()));
+					entityLiving.getPosZ(), getDrop(player, runes));
 			player.world.addEntity(dirt);
 		}
 	}
 
-	private Item getDrop() {
-		return DROPS.get(random.nextInt(DROPS.size()));
+	private ItemStack getDrop(PlayerEntity player, Set<ItemStack> runes) {
+		World world = player.world;
+		ItemStack drop = new ItemStack(DROPS.get(random.nextInt(DROPS.size())));
+		
+		// Auto-smelt
+		if (player.getRNG().nextDouble() < getEnchantmentLevel(Enchantments.FIRE_ASPECT, runes) * 0.05) {
+			IInventory inv = new Inventory(drop);
+			Optional<ItemStack> smelted = world.getRecipeManager().getRecipe(IRecipeType.SMELTING, inv, world).map(r -> r.getRecipeOutput());
+			if (smelted.isPresent())
+				drop = smelted.get();
+		}
+
+		return drop;
 	}
 
 	@Override
 	public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
-		return enchantment == Enchantments.FORTUNE;
+		return ENCHANTS.contains(enchantment);
 	}
 
 }
