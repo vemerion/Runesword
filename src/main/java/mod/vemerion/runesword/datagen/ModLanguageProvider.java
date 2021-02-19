e7f2ee6fc7963085fa8a424ad7b1f668693271f2
vemerion <vemerion@gmail.com>
2021-02-19 16:52:30 +0100

    Add guide chapters

--- src/main/java/mod/vemerion/runesword/datagen/ModLanguageProvider.java
+++ src/main/java/mod/vemerion/runesword/datagen/ModLanguageProvider.java
@@ -28,14 +28,85 @@ public class ModLanguageProvider extends LanguageProvider {
 		add("itemGroup." + Main.MODID, "Runesword");
 		addGui("guide", "Runesword");
 		addGui("home", "Home");
-		addGui("text1", "This is some text! This is another sentence. More more more. This should be enough.");
-		addGui("text2", "EVENT MORE TEXT!");
+
+		guide();
 	}
-	
+
+	private void guide() {
+		addGui("intro",
+				"Welcome to the Runesword guide, containing all the information you need to become proficient in runeforging! Click on one of the icons above to learn more about a specific subject.");
+		addGui("runeforge1", "The runeforge is the block used to insert runes into swords. Here is how you craft it:");
+		addGui("runeforge2", "And here is the runeforge GUI:");
+		addGui("runeforge3",
+				"You start by inserting a sword into the middle slot (1). You can then insert runes into the sword by placing them in slot 2-5. The higher the quality of the sword, the more runes can be inserted. For example, a stone sword only has the first rune slot unlocked (2), while a netherite sword has all four rune slots unlocked.");
+		addGui("runeforge4",
+				"The rune slots 2-4 are minor rune slots, while the top slot (5) is a major rune slot. Runes in the minor slots will have a smaller effect, while the rune in the major slot will have a big effect.");
+		addGui("enchanting", "Rune Enchanting");
+		addGui("enchantingtext",
+				"Like other tools, runes can be enchanted in an enchanting table to boost the effect of the runes. Some enchantments increase the power of the major effect, while some enchantments increase the power of the minor effect. You can read about the different possible enchantments for every type of rune in their respective guide chapter.");
+
+		addGui("blood.obtain", "Obtain: Low drop chance from all mobs.");
+		addGui("blood.minor", "Minor: Chance to heal on kill.");
+		addGui("blood.major", "Major: Attacks cost health, but deal more damage.");
+		addGui("minorenchants", "Minor Enchantments");
+		addGui("majorenchants", "Major Enchantments");
+		addGui("blood.minorenchant1", "Fire Aspect: More lifesteal if enemy on fire.");
+		addGui("blood.minorenchant2", "Aqua Affinity: More lifesteal if in water.");
+		addGui("blood.minorenchant3", "Mending: chance to repair sword on attack.");
+		addGui("blood.majorenchant1", "Protection: Chance to not drain your health when attacking.");
+		addGui("blood.majorenchant2", "Sharpness: More damage.");
+
+		addGui("air.obtain", "Obtain: Dropped by phantoms.");
+		addGui("air.minor", "Minor: Chance to knock up enemies when attacking.");
+		addGui("air.major", "Major: Grants speed boost on kill.");
+		addGui("air.minorenchant1", "Knockback: Adds knockback to attacks.");
+		addGui("air.minorenchant2", "Punch: Increase knock-up height.");
+		addGui("air.majorenchant1", "Infinity: Double speed duration.");
+		addGui("air.majorenchant2", "Efficiency: Chance to get speed II.");
+		addGui("air.majorenchant3", "Feather Falling: Chance to get Jump Boost.");
+
+		addGui("earth.obtain", "Obtain: Found in treasure chests underground.");
+		addGui("earth.minor", "Minor: Mobs have a chance to drop ores on kill.");
+		addGui("earth.major", "Major: The sword deals more damage while you are underground.");
+		addGui("earth.minorenchant1", "Fortune: Increaes ore drop chance.");
+		addGui("earth.minorenchant2", "Looting: Chance that enemy drops rare ore.");
+		addGui("earth.minorenchant3", "Fire Aspect: Chance to smelt ore immediately.");
+		addGui("earth.majorenchant1", "Shaprness: More damage underground.");
+		addGui("earth.majorenchant2", "Protection: Take reduced damage underground.");
+
+		addGui("water.obtain", "Obtain: Found in treasure chests under water.");
+		addGui("water.minor", "Minor: Restore breath on kill.");
+		addGui("water.major", "Major: The sword deals more damage while under water.");
+		addGui("water.minorenchant1", "Respiration: Increase bubble restoration.");
+		addGui("water.minorenchant2", "Luck of the Sea: Chance to ignore drowning.");
+		addGui("water.majorenchant1", "Aqua Affinity: Major effect also in rain.");
+		addGui("water.majorenchant2", "Sharpness: Increase bonus damage chance.");
+		addGui("water.majorenchant3", "Protection: Take reduced damage in water.");
+
+		addGui("fire.obtain", "Obtain: Dropped by blazes.");
+		addGui("fire.minor", "Minor: Chance to put ground on fire when attacking mob.");
+		addGui("fire.major", "Major: The sword deals more damage if the user is on fire.");
+		addGui("fire.minorenchant1", "Flame: Increase fire creation chance.");
+		addGui("fire.minorenchant2", "Power: Chance to create lava instead of fire.");
+		addGui("fire.minorenchant3", "Mending: Repair sword if hit burning mob.");
+		addGui("fire.majorenchant1", "Fire Aspect: More Damage.");
+		addGui("fire.majorenchant2", "Fire Protection: Chance to block fire damage.");
+
+		addGui("frost.obtain", "Obtain: Created by crafting Air and Water Rune together.");
+		addGui("frost.minor", "Minor: Chance to shoot slowing Frostball when attacking.");
+		addGui("frost.major", "Major: Chance to summon Frost Golem when killing enemies.");
+		addGui("frost.minorenchant1", "Multishot: Increase Frostball chance.");
+		addGui("frost.minorenchant2", "Knockback: Increase Frostball knockback.");
+		addGui("frost.minorenchant3", "Channeling: Increase slow duration.");
+		addGui("frost.majorenchant1", "Fortune: Increase Frost Golem spawn chance.");
+		addGui("frost.majorenchant2", "Infinity: Double Frost Golem duration.");
+		addGui("frost.majorenchant3", "Efficiency: Frost Golem can shoot Frostballs.");
+	}
+
 	private void addGui(String suffix, String text) {
 		addText("gui", suffix, text);
 	}
-	
+
 	private void addText(String prefix, String suffix, String text) {
 		add(prefix + "." + Main.MODID + "." + suffix, text);
 	}
--- src/main/java/mod/vemerion/runesword/screen/GuideScreen.java
+++ src/main/java/mod/vemerion/runesword/screen/GuideScreen.java
@@ -6,6 +6,7 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 
 import mod.vemerion.runesword.Main;
 import mod.vemerion.runesword.item.RuneItem;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.SimpleSound;
 import net.minecraft.client.audio.SoundHandler;
@@ -31,7 +32,7 @@ public class GuideScreen extends Screen {
 	private static final int X_OFFSET = 10;
 	private static final int CHAPTER_WIDTH = X_SIZE - X_OFFSET * 2;
 	private static final int CHAPTER_HEIGHT = Y_SIZE - Y_OFFSET * 2;
-	private static final int SCROLL_LENGTH = (int) (CHAPTER_HEIGHT * 0.6);
+	private static final int SCROLL_LENGTH = (int) (CHAPTER_HEIGHT * 0.3);
 	private static final int BUTTON_SIZE = 32;
 
 	private GuideChapter current;
@@ -143,17 +144,60 @@ public class GuideScreen extends Screen {
 		return new ResourceLocation(Main.MODID, "textures/guide/" + name + ".png");
 	}
 
-	public static final GuideChapter BLOOD_CHAPTER = new GuideChapter(() -> RuneItem.BLOOD_RUNE_ITEM,
-			RuneItem.BLOOD_RUNE_ITEM.getName()).addText(transKey("text1"));
+	public static final GuideChapter RUNEFORGE_CHAPTER = new GuideChapter(Main.RUNEFORGE_BLOCK,
+			Main.RUNEFORGE_BLOCK.getTranslatedName()).addText(transKey("runeforge1"))
+					.addImage(image("runeforge_crafting"), 518, 265).addText(transKey("runeforge2"))
+					.addImage(image("runeforge"), 176, 166).addText(transKey("runeforge3"))
+					.addText(transKey("runeforge4"));
+
+	public static final GuideChapter ENCHANT_CHAPTER = new GuideChapter(Blocks.ENCHANTING_TABLE,
+			new TranslationTextComponent(transKey("enchanting"))).addText(transKey("enchantingtext"));
+
+	public static final GuideChapter BLOOD_CHAPTER = new GuideChapter(RuneItem.BLOOD_RUNE_ITEM,
+			RuneItem.BLOOD_RUNE_ITEM.getName()).addText(transKey("blood.obtain")).addText(transKey("blood.minor"))
+					.addText(transKey("blood.major")).addHeader(transKey("minorenchants"))
+					.addText(transKey("blood.minorenchant1")).addText(transKey("blood.minorenchant2"))
+					.addText(transKey("blood.minorenchant3")).addHeader(transKey("majorenchants"))
+					.addText(transKey("blood.majorenchant1")).addText(transKey("blood.majorenchant2"));
+
+	public static final GuideChapter AIR_CHAPTER = new GuideChapter(RuneItem.AIR_RUNE_ITEM,
+			RuneItem.AIR_RUNE_ITEM.getName()).addText(transKey("air.obtain")).addText(transKey("air.minor"))
+					.addText(transKey("air.major")).addHeader(transKey("minorenchants"))
+					.addText(transKey("air.minorenchant1")).addText(transKey("air.minorenchant2"))
+					.addHeader(transKey("majorenchants")).addText(transKey("air.majorenchant1"))
+					.addText(transKey("air.majorenchant2")).addText(transKey("air.majorenchant3"));
+
+	public static final GuideChapter EARTH_CHAPTER = new GuideChapter(RuneItem.EARTH_RUNE_ITEM,
+			RuneItem.EARTH_RUNE_ITEM.getName()).addText(transKey("earth.obtain")).addText(transKey("earth.minor"))
+					.addText(transKey("earth.major")).addHeader(transKey("minorenchants"))
+					.addText(transKey("earth.minorenchant1")).addText(transKey("earth.minorenchant2"))
+					.addText(transKey("earth.minorenchant3")).addHeader(transKey("majorenchants"))
+					.addText(transKey("earth.majorenchant1")).addText(transKey("earth.majorenchant2"));
+
+	public static final GuideChapter WATER_CHAPTER = new GuideChapter(RuneItem.WATER_RUNE_ITEM,
+			RuneItem.WATER_RUNE_ITEM.getName()).addText(transKey("water.obtain")).addText(transKey("water.minor"))
+					.addText(transKey("water.major")).addHeader(transKey("minorenchants"))
+					.addText(transKey("water.minorenchant1")).addText(transKey("water.minorenchant2"))
+					.addHeader(transKey("majorenchants")).addText(transKey("water.majorenchant1"))
+					.addText(transKey("water.majorenchant2")).addText(transKey("water.majorenchant3"));
+
+	public static final GuideChapter FIRE_CHAPTER = new GuideChapter(RuneItem.FIRE_RUNE_ITEM,
+			RuneItem.FIRE_RUNE_ITEM.getName()).addText(transKey("fire.obtain")).addText(transKey("fire.minor"))
+					.addText(transKey("fire.major")).addHeader(transKey("minorenchants"))
+					.addText(transKey("fire.minorenchant1")).addText(transKey("fire.minorenchant2"))
+					.addText(transKey("fire.minorenchant3")).addHeader(transKey("majorenchants"))
+					.addText(transKey("fire.majorenchant1")).addText(transKey("fire.majorenchant2"));
+
+	public static final GuideChapter FROST_CHAPTER = new GuideChapter(RuneItem.FROST_RUNE_ITEM,
+			RuneItem.FROST_RUNE_ITEM.getName()).addText(transKey("frost.obtain")).addText(transKey("frost.minor"))
+					.addText(transKey("frost.major")).addHeader(transKey("minorenchants"))
+					.addText(transKey("frost.minorenchant1")).addText(transKey("frost.minorenchant2"))
+					.addText(transKey("frost.minorenchant3")).addHeader(transKey("majorenchants"))
+					.addText(transKey("frost.majorenchant1")).addText(transKey("frost.majorenchant2"))
+					.addText(transKey("frost.majorenchant3"));
 
 	public static final GuideChapter START_CHAPTER = new GuideChapter(() -> Main.GUIDE_ITEM,
-			new TranslationTextComponent(transKey("guide"))).addChild(BLOOD_CHAPTER).addText(transKey("text1"))
-					.addText(transKey("text2")).addText(transKey("text2")).addText(transKey("text2"))
-					.addText(transKey("text2")).addText(transKey("text2")).addHeader(transKey("text1"))
-					.addText(transKey("text2")).addImage(image("runeforge_crafting"), 518, 264)
-					.addText(transKey("text2")).addHeader(transKey("text2")).addText(transKey("text2"))
-					.addText(transKey("text2")).addText(transKey("text2")).addText(transKey("text2"))
-					.addText(transKey("text2")).addText(transKey("text2")).addText(transKey("text2"))
-					.addText(transKey("text2")).addText(transKey("text2")).addText(transKey("text2"))
-					.addText(transKey("text2")).addText(transKey("text2"));
+			new TranslationTextComponent(transKey("guide"))).addChild(RUNEFORGE_CHAPTER).addChild(ENCHANT_CHAPTER)
+					.addChild(BLOOD_CHAPTER).addChild(AIR_CHAPTER).addChild(EARTH_CHAPTER).addChild(WATER_CHAPTER)
+					.addChild(FIRE_CHAPTER).addChild(FROST_CHAPTER).addText(transKey("intro"));
 }
